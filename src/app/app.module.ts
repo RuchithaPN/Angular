cf28d934881b201dd09b24dc3086da560845cfc2
RuchithaPN <117449601+RuchithaPN@users.noreply.github.com>
2024-01-30 11:54:58 +0530

    usecase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { NotificationComponent } from './notification/notification.component';
-import { ContactComponent } from './contact/contact.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaginatorComponent } from './paginator/paginator.component';
+import { FilterComponent } from './filter/filter.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoginComponent,
-    NotificationComponent,
-    ContactComponent,
-    GalleryComponent
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaginatorComponent,
+    FilterComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
